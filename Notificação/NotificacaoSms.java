5f69fa268c700d1477b9ce293e6952b7f3338e87
guilhermeahs <guigui010110@gmail.com>
2026-06-09 21:24:53 -0300

    Projeto notificação

---
+++ Notificação/NotificacaoSms.java
@@ -0,0 +1,6 @@
+public class NotificacaoSms implements Notificacao {
+    @Override
+    public void enviar(String mensagem) {
+        System.out.println("Enviando SMS: " + mensagem);
+    }
+}
---
+++ Notificação/ServicoNotificacao.java
@@ -0,0 +1,5 @@
+public class ServicoNotificacao {
+    public void notificarCliente(Notificacao notificacao, String mensagem) {
+        notificacao.enviar(mensagem);
+    }
+}
